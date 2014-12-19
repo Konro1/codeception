37bd3f20c84eee61ee9d9318fad9e539d6c45c8a
Yuriy Myrosh <yuriy.myrosh@lemberg.co.uk>
2014-12-19 16:47:05 +0200

    added photo for uploads testing

--- tests/functional/UploadCept.php
+++ tests/functional/UploadCept.php
@@ -6,7 +6,7 @@ $I->amOnPage('/');
 $I->see('complete');
 
 //all of this doesnt work
-$I->fillField('textarea[name^="captions"]', 'test');
+
 $I->fillField('textarea[name="captions[]"]', 'test');
 $I->fillField('//textarea[@name="captions[]"]', 'test');
 $I->fillField('.caption', 'test');
